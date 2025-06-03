360f9fa1c09794e38b642cdbd08f927a6387f674
Nanashi. <sevenc7c@sevenc7c.com>
2025-06-03 13:09:42 +0900

    fix: LFSを後に持ってくる

--- setup.bash
+++ setup.bash
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-# lfsを設定
-echo "Setting up Git LFS..."
-git lfs install
-git lfs pull
-
 # gitに自作credential helperを設定
 RUBY_PATH=$(which ruby)
 echo "Using Ruby at: $RUBY_PATH"
 git config set --all credential.helper "cache --timeout=$($RUBY_PATH ./credential_helper.rb fetch_timeout)"
 git config set --append credential.helper "$RUBY_PATH ./credential_helper.rb"
+
+# lfsを設定
+echo "Setting up Git LFS..."
+git lfs install
+git lfs pull
